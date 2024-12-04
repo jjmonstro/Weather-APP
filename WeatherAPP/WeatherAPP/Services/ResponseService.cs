08d5e0980a9338cc6c737894b678bb38cc285daa
jjmonstro <jpcorreia1211@gmail.com>
2024-12-04 16:32:37 -0300

    everything working (tudo funcionando)

--- WeatherAPP/WeatherAPP/Services/ResponseService.cs
+++ WeatherAPP/WeatherAPP/Services/ResponseService.cs
@@ -15,8 +15,8 @@ namespace WeatherAPP.Services
         private HttpClient httpClient;
         private FullResponse fullResponse; //isso aqui é para get byid que só traz uma
         private JsonSerializerOptions jsonSerializerOptions; // configurar/formatar o JSON
-        Uri uri = new Uri("http://api.openweathermap.org/data/2.5/weather?q=");
-        String key = "APPID=ab73f17bcbdd1656c1518494388ce6e8";
+        Uri uri = new Uri("http://api.openweathermap.org/data/2.5/weather?");
+        String key = "appid=ab73f17bcbdd1656c1518494388ce6e8";
         String options = "lang=pt_br&units=metric";
         public ResponseService()
         {
@@ -40,7 +40,7 @@ namespace WeatherAPP.Services
 
             try
             {
-                HttpResponseMessage response = await httpClient.GetAsync($"{uri}{cidade}&{options}&{key}");//quero saber todos os posts;
+                HttpResponseMessage response = await httpClient.GetAsync($"{uri}q={cidade}&{options}&{key}");//quero saber todos os posts;
                 if (response.IsSuccessStatusCode)
                 {
                     string content = await response.Content.ReadAsStringAsync();// tranforma o conteudo em string;
@@ -70,5 +70,39 @@ namespace WeatherAPP.Services
         }
 
 
+        public async Task<FullResponse> GetResponseByCoordAsync(String lat, String lon) // TASK: usado no await
+        {
+            Debug.WriteLine("Chamou!! o GetResponseByCoordAsync");
+
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync($"{uri}lat={lat}&lon={lon}&{options}&{key}");//quero saber todos os posts;
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();// tranforma o conteudo em string;
+
+                    Debug.WriteLine($"Resposta JSON: {content}");
+
+                    fullResponse = JsonSerializer.Deserialize<FullResponse>(content, jsonSerializerOptions);
+                }
+                else
+                {
+                    // se der erro na chama da API mostra
+                    Debug.WriteLine($"Erro na chamada à API: {response.StatusCode}");
+                }
+            }
+            catch (JsonException ex)
+            {
+                // se der alguma exeption ai mostra
+                Debug.WriteLine($"Exceção ocorrida: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                // se der alguma exeption ai mostra
+                Debug.WriteLine($"Exceção ocorrida: {ex.Message}");
+            }
+
+            return fullResponse;
+        }
     }
 }
--- WeatherAPP/WeatherAPP/ViewModels/MainViewModel.cs
+++ WeatherAPP/WeatherAPP/ViewModels/MainViewModel.cs
@@ -35,30 +35,72 @@ namespace WeatherAPP.ViewModels
         [ObservableProperty]
         public String umidade;
 
+        [ObservableProperty]
+        public string imagem;
+
+
         private ResponseService responseService = new ResponseService();
 
         public ICommand BuscarCommand { get; set; }
+
+
         public MainViewModel()
         {
             BuscarCommand = new Command(Buscar);
+            GetaLatLongAsync();
         }
 
         public async void Buscar()
         {
-            if (Cidade==null)
+            if (Cidade == null)
             {
                 Debug.WriteLine("Cidade ta nulo");
                 return;
             }
             FullResponse fullResponse = await responseService.GetResponseByCidadeAsync(Cidade);
 
-            Descricao = "Descrição: " + fullResponse.Weather[0].Description;
-            Temperatura = "Temperatura atual: " + fullResponse.Main.Temp.ToString() + " °C";
-            TemperaturaMax = "Temperatura máxima: " + fullResponse.Main.TempMax.ToString() + " °C";
-            TemperaturaMin = "Temperatura mínima: " + fullResponse.Main.TempMin.ToString() + " °C";
-            Sensacao = "Sensação térmica: " + fullResponse.Main.FeelsLike.ToString() + " °C";
-            Umidade = "Umidade: " + fullResponse.Main.Humidity.ToString() + "%";
-            
+            AtualizarTela(fullResponse);
+        }
+
+        //esquema de pegar a localização
+        public async Task<(double Latitude, double Longitude)> GetaLatLongAsync()
+        {
+            try
+            {
+                
+                var location = await Geolocation.GetLocationAsync(new GeolocationRequest
+                {
+                    DesiredAccuracy = GeolocationAccuracy.Medium,
+                    Timeout = TimeSpan.FromSeconds(10)
+                });
+
+                if (location != null)
+                {
+                    Debug.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}");
+                    FullResponse fullResponse = await responseService.GetResponseByCoordAsync(location.Latitude.ToString(),location.Longitude.ToString());
+                    AtualizarTela(fullResponse);
+                    return (location.Latitude, location.Longitude);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Erro ao obter localização: {ex.Message}");
+            }
+
+            // Retorna coordenadas inválidas caso não consiga obter a localização
+            return (0, 0);
+        }
+
+        public void AtualizarTela(FullResponse fullResponse)
+        {
+            Descricao = fullResponse.Weather[0].Description;
+            Temperatura = fullResponse.Main.Temp.ToString() + " °C";
+            TemperaturaMax = fullResponse.Main.TempMax.ToString() + " °C";
+            TemperaturaMin = fullResponse.Main.TempMin.ToString() + " °C";
+            Sensacao = fullResponse.Main.FeelsLike.ToString() + " °C";
+            Umidade = fullResponse.Main.Humidity.ToString() + "%";
+            Imagem = fullResponse.Weather[0].Main.ToLower() + ".png";
+            Debug.WriteLine(Imagem);
         }
     }
 }
